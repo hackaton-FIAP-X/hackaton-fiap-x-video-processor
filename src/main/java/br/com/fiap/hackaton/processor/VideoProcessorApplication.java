c4b3e2592c55a4f8355ff4c21b848bc0219df711
denis rodrigues <denisrodrigues.ita@gmail.com>
2026-08-26 19:48:14 -0300

    first commit

---
+++ src/main/java/br/com/fiap/hackaton/processor/VideoProcessorApplication.java
@@ -0,0 +1,12 @@
+package br.com.fiap.hackaton.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoProcessorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VideoProcessorApplication.class, args);
+  }
+}
---
+++ src/test/java/br/com/fiap/hackaton/processor/VideoProcessorApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.fiap.hackaton.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessorApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
